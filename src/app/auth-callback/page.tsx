9f6d62589846562f7773310c6354f085a6293c73
eltanin33 <finalodysseyxzy@gmail.com>
2024-05-16 22:45:00 +0800

    feat: tRPC setup

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,18 @@
+import { useRouter, useSearchParams } from "next/navigation";
+import { trpc } from "../_trpc/client";
+
+const Page = async () => {
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const origin = searchParams.get("origin");
+
+  const { data, isLoading, isSuccess } = trpc.authCallback.useQuery();
+
+  if (isSuccess) {
+    // user is synced to db
+    router.push(origin ? `/${origin}` : "/dashboard");
+  }
+};
+
+export default Page;
