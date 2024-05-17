69862c3a18f009d3d01a804840bef4e9ada1f689
eltanin33 <finalodysseyxzy@gmail.com>
2024-05-17 15:13:17 +0800

    feat: user db sync

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,18 +1,37 @@
+"use client";
+
 import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/client";
+import { Loader2 } from "lucide-react";
 
-const Page = async () => {
+const Page = () => {
   const router = useRouter();
 
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
 
-  const { data, isLoading, isSuccess } = trpc.authCallback.useQuery();
+  const { data, isLoading, isSuccess, isError, error } =
+    trpc.authCallback.useQuery(undefined, { retry: true, retryDelay: 500 });
 
   if (isSuccess) {
     // user is synced to db
     router.push(origin ? `/${origin}` : "/dashboard");
   }
+
+  if (isError) {
+    if (error.data?.code === "UNAUTHORIZED") {
+      router.push("/sign-in");
+    }
+  }
+  return (
+    <div className="w-fukll mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Setting up yout account</h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,6 +1,7 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -9,8 +10,24 @@ export const appRouter = router({
     if (!user || !user.id || !user.email)
       throw new TRPCError({ code: "UNAUTHORIZED" });
 
+    // check if the user is in the db
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+
+    if (!dbUser) {
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
+
     return { success: true };
   }),
 });
-  
+
 export type AppRouter = typeof appRouter;
