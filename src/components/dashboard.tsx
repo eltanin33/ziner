1e9dbaa01ae10045006f1d30d2597a1bcd25fccc
eltanin33 <finalodysseyxzy@gmail.com>
2024-05-17 16:56:17 +0800

    feat: file list mutation

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -2,14 +2,38 @@
 
 import { trpc } from "@/app/_trpc/client";
 import UploadButton from "./upload-button";
-import { Ghost, MessageSquare, Plus, Trash, TrashIcon } from "lucide-react";
+import {
+  Ghost,
+  Loader2,
+  MessageSquare,
+  Plus,
+  Trash,
+  TrashIcon,
+} from "lucide-react";
 import Skeleton from "react-loading-skeleton";
 import Link from "next/link";
 import { format } from "date-fns";
 import { Button } from "./ui/button";
+import { useState } from "react";
 
 const Dashboard = () => {
+  const [currentlyDeleting, setCurrentlyDeleting] = useState<string | null>();
+
   const { data: files, isLoading } = trpc.getUserFiles.useQuery();
+  const utils = trpc.useUtils();
+
+  const { mutate: deleteFile } = trpc.deleteFile.useMutation({
+    onSuccess: () => {
+      utils.getUserFiles.invalidate();
+    },
+    onMutate: ({ id }) => {
+      setCurrentlyDeleting(id);
+    },
+    onSettled: () => {
+      setCurrentlyDeleting(null);
+    },
+  });
+
   return (
     <main className="mx-auto max-w-7xl md:p-10">
       <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row">
@@ -57,8 +81,17 @@ const Dashboard = () => {
                     mocked
                   </div>
 
-                  <Button size="sm" className="w-full" variant="destructive">
-                    <Trash className="h-4 w-4" />
+                  <Button
+                    onClick={() => deleteFile({ id: file.id })}
+                    size="sm"
+                    className="w-full"
+                    variant="destructive"
+                  >
+                    {currentlyDeleting === file.id ? (
+                      <Loader2 className="h-4 w-4 animate-spin" />
+                    ) : (
+                      <Trash className="h-4 w-4" />
+                    )}
                   </Button>
                 </div>
               </li>
